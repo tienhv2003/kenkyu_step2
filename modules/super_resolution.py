fa267ca16e8c10350d3332036e9908e19d009e97
HOANG VAN TIEN <hoangvantien252003@gmail.com>
2025-10-10 13:39:02 +0700

    update code 10102025

--- modules/super_resolution.py
+++ modules/super_resolution.py
@@ -3,14 +3,11 @@ import cv2.dnn_superres
 import numpy as np
 import os
 
-
 #AIモデルの読み込み
 model_FSRCNN = cv2.dnn_superres.DnnSuperResImpl_create()
 
 #モデルの設定
-
 model_FSRCNN.readModel("models/super_resolution/FSRCNN_x4.pb")
-
 model_FSRCNN.setModel("fsrcnn", 4)
 
 #画像フォルダ内のすべての画像を処理する関数
@@ -26,7 +23,9 @@ def superres_images_in_folder(input_folder, output_folder):
         if img is None:
             print(f"画像を読み込めませんでした: {input_path}")
             continue
+        
         upscaled_img = model_FSRCNN.upsample(img)
+        
         cv2.imwrite(output_path, upscaled_img)
         print(f"超解像画像を保存しました: {output_path}")
 
